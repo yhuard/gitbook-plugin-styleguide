2dd72a82d762c8114114fe4ea09992000d0641dc
Yannick Huard <huard.yan@gmail.com>
2016-01-28 14:28:53 +0100

    UI improvements
    UI improvements

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 function iframeLoaded(el) {
   $(el).removeClass('is-loading');
-  $(el).iFrameResize();
+  $(el).iFrameResize({
+    heightCalculationMethod: 'max',
+    autoResize: false
+  });
 }
+
+$('.styleguide .dropdown-menu a').on('click', function (event) {
+    $(this).parents('.dropdown').toggleClass('open');
+});
